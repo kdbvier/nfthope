5df0f2f85a659f69f7374258793539e9a61aee31
kdbvier <kdbvier@gmail.com>
2022-05-11 02:03:47 +0800

    sd

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { Wrapper,SubWrapper,ImgWrapper,StyledButton } from "./styled";
+import { Wrapper,SubWrapper,ImgWrapper } from "./styled";
 
-import { useAppSelector } from "../../app/hooks";
-import useFetch from "../../hook/useFetch";
 import home from "../../assets/images/home.png";
 
 const Home: React.FC = () => {
@@ -14,11 +12,6 @@ const Home: React.FC = () => {
         <span style={{fontSize:"24px", textAlign:"left"}}>
         On Hopers.io the DAO governs and earns rewards through staking the $HOPE token with a community driven revshare model
         </span>
-        <div>
-          {/* <StyledButton>
-
-          </StyledButton> */}
-        </div>
       </SubWrapper>
       <ImgWrapper src={home} alt="home" />
     </Wrapper>
