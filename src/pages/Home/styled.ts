7a893dbb62cc16b85950b4212651066a1b48657f
sheriff-rango <95250818+sheriff-rango@users.noreply.github.com>
2022-06-07 08:53:57 +0800

    modified content of home page and image size of nft item

--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -2,32 +2,40 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   padding: 10px 0;
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   width: 80%;
   justify-content: center;
-  margin:0 auto;
+  margin: 0 auto;
   @media (max-width: 650px) {
     width: 100%;
-    flex-direction: column;
+    grid-template-columns: 1fr;
   }
 `;
 
 export const SubWrapper = styled.div`
   padding: 20px;
-  display:flex;
+  display: flex;
   flex-direction: column;
   align-self: center;
-`
+`;
 
 export const ImgWrapper = styled.img`
   height: 550px;
-  width: 550px;
+  /* width: 550px; */
   @media (max-width: 650px) {
-    width: 100%;
+    /* width: 100%; */
     height: 100%;
   }
-`
+`;
 
-export const StyledButton = styled.button`
+export const StyledButton = styled.button``;
 
-`
+export const MainContent = styled.h1`
+  text-align: center;
+`;
+
+export const SubContent = styled.span`
+  font-size: 24px;
+  text-align: center;
+`;
