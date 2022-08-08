58cbf7e13c22259a12206d677d2d3db9aeb897d9
sheriff-rango <95250818+sheriff-rango@users.noreply.github.com>
2022-08-08 23:44:09 +0800

    added token price chart to home page

--- src/components/PriceStatistic/styled.ts
+++ src/components/PriceStatistic/styled.ts
@@ -1,12 +1,25 @@
 import styled from "styled-components";
+import Select from "react-select";
 
 export const Wrapper = styled.div`
   width: 100%;
   margin: 150px 0 50px;
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
   overflow: auto;
+`;
+
+export const ChartArea = styled.div`
+  width: 50%;
+  min-width: 500px;
+  height: 350px;
+  position: relative;
 `;
 
 export const Title = styled.div`
@@ -27,6 +40,11 @@ export const PriceStatisticItem = styled.div`
   grid-template-columns: 70px auto 50px 50px 50px 50px;
   grid-gap: 20px;
   align-items: center;
+  border-top: 1px solid ${({ theme }) => theme.colors.fontColor};
+  padding: 10px;
+  &:last-child {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.fontColor};
+  }
   @media (max-width: 450px) {
     grid-template-columns: 60px auto 50px 50px 50px 50px;
     grid-gap: 5px;
@@ -72,4 +90,13 @@ export const Span = styled.span<{
   color: ${({ color }) => color ?? ""};
   font-size: ${({ fontSize }) => fontSize ?? ""};
   font-weight: ${({ fontWeight }) => fontWeight ?? ""};
+`;
+
+export const StyledSelect = styled(Select)`
+  width: max-content;
+  position: absolute !important;
+  z-index: 1;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
 `;
